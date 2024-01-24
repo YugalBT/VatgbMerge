279eb986057fcdd5a28d9e04aa8675b4d9d752a5
Yugal Mittal <yugal@braintechnosys.com>
2024-01-24 18:52:59 +0530

    Minor issue changes in update forign checks

--- Standing_Order_Vat_App.Common/ViewModels/CheckSettlementBatchVm.cs
+++ Standing_Order_Vat_App.Common/ViewModels/CheckSettlementBatchVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,16 @@ namespace Standing_Order_Vat_App.Common.ViewModels
     public class CheckSettlementBatchVm
     {
         public long SettlementId { get; set; }
+        [Display(Name ="Check Number")]
         public string CheckNumber{ get; set; }
+        [Display(Name = "Settlement Amount")]
         public decimal? SettlementAmount { get; set; }
         public long BatchId { get; set; }
+        [Display(Name = "Date Settled")]
         public DateTime? DateSettled { get; set; }
+        [Display(Name = "Settle By Wire")]
         public string SettledByWire { get; set; }
+        [Display(Name = "Bank")]
         public string BankName { get; set; }
         
         //public List<BankListVm> BanksList { get; set; }
