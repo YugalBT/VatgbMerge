fff96e56f75373ec8d0c4238f2dec9c4f1f336e1
Brajesh <brajesh@braintechnosys.com>
2025-01-08 14:56:46 +0530

    Changes in Common Method

---
+++ Standing_Order_Vat_App/MvcHelper/CalculationForNetFee.cs
@@ -0,0 +1,10 @@
+﻿namespace Standing_Order_Vat_App.MvcHelper
+{
+    public static class CalculationForNetFee
+    {
+        public static double CalculateNetFee(double transAmt)
+        {
+            return Math.Round((transAmt / 1.17), 2);
+        }
+    }
+}
