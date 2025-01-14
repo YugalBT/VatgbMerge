797422f2e6b346e9d179387ef75d4910bdf3b424
yugal <yugal@braintechnosys.com>
2025-01-14 18:09:46 +0530

    changes in vat service

--- Standing_Order_Vat_App.DAL/GB_Register/TblVatApply.cs
+++ Standing_Order_Vat_App.DAL/GB_Register/TblVatApply.cs
@@ -9,9 +9,9 @@ namespace Standing_Order_Vat_App.DAL.GB_Register
     public class TblVatApply
     {
         public int Id { get; set; }
-        public decimal? VatApplyValue { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public bool? IsActive { get; set; }
+        public decimal VatApplyValue { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
     }
 }
